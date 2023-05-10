1787eaa5dd7d3402d8cbe529782ec2e5f1598c7b
Nhu Quan (Jim) Tran <quantrannhu@gmail.com>
2023-05-10 20:39:19 +0000

    Modified to remove unreachable code- javascript-conditionals - Jimmy Tran

--- javascript-conditionals/main.js
+++ javascript-conditionals/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unreachable */
 /* exported isUnderFive,
             isEven,
             startsWithJ,
@@ -8,37 +9,44 @@
             introduceWarnerBro,
             recommendMovie
  */
-const isUnderFive=number=>number<5;
-const isEven=number=>number%2===0;
-const startsWithJ=string=>string[0]==='J';
-const isOldEnoughToDrink=({name,age})=>age>=21;
-const isOldEnoughToDrive=({name,age})=>age>=16;
-const isOldEnoughToDrinkAndDrive=({name,age})=>false; // dont drink AND drive, drink THEN drive:)
-function categorizeAcidity(pH){
-if(pH===7)return 'neutral';
-else if(pH>0&&pH<7) return 'acid';
-else if(pH>7&&pH<14) return 'base';
-else return 'invalid pH level';
+const isUnderFive = (number) => number < 5;
+const isEven = (number) => number % 2 === 0;
+const startsWithJ = (string) => string[0] === 'J';
+const isOldEnoughToDrink = ({ name, age }) => age >= 21;
+const isOldEnoughToDrive = ({ name, age }) => age >= 16;
+const isOldEnoughToDrinkAndDrive = ({ name, age }) => false; // dont drink AND drive, drink THEN drive:)
+function categorizeAcidity(pH) {
+  if (pH === 7) return 'neutral';
+  else if (pH > 0 && pH < 7) return 'acid';
+  else if (pH > 7 && pH < 14) return 'base';
+  else return 'invalid pH level';
 }
-const introduceWarnerBro=name=>{
-  switch (name){
+const introduceWarnerBro = (name) => {
+  switch (name) {
     case 'yakko':
     case 'wakko':
-      return "We're the warner brothers!"; break;
+      return "We're the warner brothers!";
     case 'dot':
-      return "I'm cute~"; break;
+      return "I'm cute~";
     default:
-      return "Goodnight everybody!"; break;
+      return 'Goodnight everybody!';
   }
-}
-function recommendMovie(genre){
-  switch(genre){
-    case 'action': return 'Die Hard'; break;
-    case 'comedy': return 'The Big Lebowski'; break;
-    case 'horror': return 'Winnie the Pooh'; break;
-    case 'drama': return 'Red Notice'; break;
-    case 'musical': return 'High School Musical'; break;
-    case 'sci-fi': return 'Alita'; break;
-    default: return 'Genre not recognized. Choose between action, comedy, horror, drama, musical, or sci-fi';break;
+};
+function recommendMovie(genre) {
+  switch (genre) {
+    case 'action':
+      return 'Die Hard';
+    case 'comedy':
+      return 'The Big Lebowski';
+    case 'horror':
+      return 'Winnie the Pooh';
+    case 'drama':
+      return 'Red Notice';
+    case 'musical':
+      return 'High School Musical';
+    case 'sci-fi':
+      return 'Alita';
+    default:
+      return 'Genre not recognized. Choose between action, comedy, horror, drama, musical, or sci-fi';
   }
 }
