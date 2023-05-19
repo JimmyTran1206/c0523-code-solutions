84198454c5888f1e5ef04579992569a40c5edd82
Nhu Quan (Jim) Tran <quantrannhu@gmail.com>
2023-05-19 18:28:13 +0000

    Completed array-manipulation-3 with all consol.log() and testing variables removed

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -10,19 +10,3 @@ const equal = (first, second) => {
   }
   return true;
 };
-
-const arr1 = [0, 1, 2, [3, 4]];
-
-console.log(arr1.flat());
-// Expected output: Array [0, 1, 2, 3, 4]
-
-const arr2 = [0, 1, 2, [[[3, 4]]]];
-
-console.log(arr2.flat(2));
-// Expected output: Array [0, 1, 2, Array [3, 4]]
-
-console.log(arr2.flat(1));
-// Expected output: Array [0, 1, 2, Array [Array [3, 4]]]
-
-console.log(arr2.flat(3));
-// Expected output: Array [0, 1, 2, 3, 4]
