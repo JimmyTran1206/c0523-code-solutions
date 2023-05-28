c298627adff147b9c146c0fc52457e172e1453eb
Nhu Quan (Jim) Tran <quantrannhu@gmail.com>
2023-05-28 01:02:28 +0000

    javascript-forms/

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -2,6 +2,7 @@ const $contactForm = document.querySelector('#contact-form');
 const formData = {};
 $contactForm.addEventListener('submit', (event) => {
   event.preventDefault();
+  // use the name and value pairs in assigning the input value to the object
   formData[$contactForm.elements[0].name] = $contactForm.elements[0].value;
   formData[$contactForm.elements[1].name] = $contactForm.elements[1].value;
   formData[$contactForm.elements[2].name] = $contactForm.elements[2].value;
