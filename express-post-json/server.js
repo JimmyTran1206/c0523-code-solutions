b913170f9f2801796cb541375a2fe03c216e9e36
Nhu Quan (Jim) Tran <quantrannhu@gmail.com>
2023-07-11 01:55:43 +0000

    Completed express-post-json

--- express-post-json/server.js
+++ express-post-json/server.js
@@ -18,7 +18,7 @@ app.post('/api/grades/', (req, res) => {
   const obj = req.body;
   obj.id = nextId++;
   grades[obj.id] = obj;
-  res.json(obj);
+  res.status(201).json(obj);
 });
 
 app.listen('8080', () => console.log('Listening to port 8080'));
