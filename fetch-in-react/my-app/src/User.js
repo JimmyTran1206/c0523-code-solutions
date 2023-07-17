81117289b2c74f61c49e2e3b7a2edd5499ba5b21
Nhu Quan (Jim) Tran <quantrannhu@gmail.com>
2023-07-17 21:49:12 +0000

    Completed fet-in-react

---
+++ fetch-in-react/my-app/src/User.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars -- Remove me */
+import { useEffect, useState } from 'react';
+import UserCard from './UserCard';
+
+export default function User({ userId, onCancel }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+  const [user, setUser] = useState();
+
+  /* your code here (hint: useEffect) */
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    console.error('Fetch error:', error);
+    return <p>Error! {error.message}</p>;
+  }
+
+  return (
+    <div>
+      <button onClick={() => onCancel()}>Close</button>
+      <UserCard user={user} />
+    </div>
+  );
+}
